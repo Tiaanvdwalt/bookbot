61896f72706c4897715fe5560bb06e810d101915
Tiaanvdwalt <tiaan.vdwalt1234@gmail.com>
2025-09-25 14:33:23 +0200

    updated stats.py

--- stats.py
+++ stats.py
@@ -11,20 +11,3 @@ def count_characters(text):
         else:
             char_count[char] = 1
     return char_count
-    
-
-
-
-
-
-''' 
-def count_characters(text):
-    char_count = {}
-    book_text = get_book_text("books/frankenstein.txt").lower()
-    for char in book_text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    return char_count
-'''
